88a7aa1335af42390db4a40c9fc518ab65e11d9a
Taras Makarchuk <2278452@gmail.com>
2023-02-13 23:48:46 +0200

    :sparkles:  client - add chakra ui npm, layout search component

--- client/app/components/layout/header/search/Search.tsx
+++ client/app/components/layout/header/search/Search.tsx
@@ -1,7 +1,30 @@
-import { FC } from 'react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { FC, useState } from 'react';
+import styles from './Search.module.scss';
 
 const Search: FC = () => {
-	return <div>Search</div>;
+	const [searchValue, setSearchValue] = useState('');
+
+	return (
+		<div className={styles.search}>
+			<div>
+				<InputGroup>
+					<InputRightElement
+						pointerEvents="none"
+						children={<SearchIcon color='gray.500' />}
+					/>
+					<Input
+						variant='flushed'
+						type='search'
+						onChange={e => setSearchValue(e.target.value)}
+						value={searchValue}
+						placeholder='SEARCH'
+					/>
+				</InputGroup>
+			</div>
+		</div>
+	);
 };
 
 export default Search;
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,12 @@
-// import '@/assets/styles/global.scss';
+import '@/assets/styles/global.scss';
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
