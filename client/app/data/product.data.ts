c09b2f286e46a00beda7a97f79f9e7c10b186376
Taras Makarchuk <2278452@gmail.com>
2023-02-15 20:24:17 +0200

    :construction: client - add product images, product data, product interface

---
+++ client/app/data/product.data.ts
@@ -0,0 +1,65 @@
+import { IProduct } from "@/types/product.interface";
+
+export const products: IProduct[] = [
+	{
+		id: 1,
+		description: 'Calories total Fat 19g milk 6%',
+		name: 'Set 2 Berry morse & Capuccino - Midnight Mint',
+		images: [
+			'/images/products/berry-morse.png',
+			'/images/products/capuccino.png',
+		],
+		price: 7.29,
+		reviews: [],
+	},
+	{
+		id: 2,
+		description: 'Calories total Fat 29g milk 5%',
+		name: 'Green tea ',
+		images: [
+			'/images/products/green-tea.png',
+		],
+		price: 4.99,
+		reviews: [],
+	},
+	{
+		id: 3,
+		description: 'Calories total Fat 10g milk 0%',
+		name: 'Lemonade',
+		images: [
+			'/images/products/lemonade.png',
+		],
+		price: 4.99,
+		reviews: [],
+	},
+	{
+		id: 4,
+		description: 'Calories total Fat 14g milk 0%',
+		name: 'Mochito',
+		images: [
+			'/images/products/mochito.png',
+		],
+		price: 4.99,
+		reviews: [],
+	},
+	{
+		id: 5,
+		description: 'Calories total Fat 14g milk 0%',
+		name: 'Mokachino',
+		images: [
+			'/images/products/mokachino.png',
+		],
+		price: 3.49,
+		reviews: [],
+	},
+	{
+		id: 6,
+		description: 'Calories total Fat 14g milk 0%',
+		name: 'Caramelle',
+		images: [
+			'/images/products/caramelle.png',
+		],
+		price: 2.99,
+		reviews: [],
+	},
+];
