f6582ad4e9afdbf0cbb7df21599dbe7a7405cc86
Taras Makarchuk <2278452@gmail.com>
2023-02-17 23:42:19 +0200

    :rocket:  client - add redux persist npm, add functionality to save cart information to local storage

--- client/app/store/store.ts
+++ client/app/store/store.ts
@@ -1,13 +1,36 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { cartSlice } from '@/store/slice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import {
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
 
 const rootReducer = combineReducers({
 	cart: cartSlice.reducer,
 });
 
-//TODO localeStorage
+const persistConfig = {
+	key: 'cart',
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export type TypeRootState = ReturnType<typeof rootReducer>;
