0f06c8f5c37f1b8d79cef98e90d22dec6d228134
Taras Makarchuk <2278452@gmail.com>
2023-02-23 21:25:14 +0200

    :white_check_mark: add choose click cart functionality and buttons next and prev, layout

--- client/app/components/ui/catalog/carousel/carousel-item/CarouselItem.tsx
+++ client/app/components/ui/catalog/carousel/carousel-item/CarouselItem.tsx
@@ -6,23 +6,55 @@ import cn from 'clsx';
 import { CarouselBtn } from '@/ui/catalog/carousel/carousel-item/CarouselBtn';
 import { CarouselVariations } from '@/ui/catalog/carousel/carousel-item/CarouselVariations';
 import { TypeSize } from '@/store/types';
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import { Button } from '@chakra-ui/react';
 
-export const CarouselItem: FC<{ product: IProduct }> = ({ product }) => {
+interface ICarouselItem {
+	product: IProduct;
+	selectItem: () => void;
+	isActive: boolean;
+	nextHandler: () => void;
+	previousHandler: () => void;
+}
+
+export const CarouselItem: FC<ICarouselItem> = ({ product, isActive, selectItem, nextHandler, previousHandler }) => {
 	const [selectedSize, setSelectedSize] = useState<TypeSize>('SHORT');
-	const isActive = product.id === 2;
 
 	return (
-		<div className={cn(styles.item, {
+		<button className={cn(styles.item, {
 			[styles.active]: isActive
-		})}>
+		})}
+		onClick={selectItem}
+		>
 
 			<div>
-				<Image
-					className={styles.image}
-					alt={product.name}
-					src={product.images[0]} width={315}
-					height={315}
-				/>
+				<div
+					className='flex items-center'
+					style={{ display: 'flex', flexDirection: 'row' }}
+				>
+					{isActive && (
+						<Button
+							className={styles['previous-button']}
+							onClick={previousHandler}
+						>
+							<ChevronLeftIcon boxSize={13} />
+						</Button>
+					)}
+					<Image
+						className={styles.image}
+						alt={product.name}
+						src={product.images[0]} width={315}
+						height={315}
+					/>
+					{isActive && (
+						<Button
+							className={styles['next-button']}
+							onClick={nextHandler}
+						>
+							<ChevronRightIcon boxSize={13} />
+						</Button>
+					)}
+				</div>
 
 				<div className={styles.heading}>
 					<div>
@@ -47,6 +79,6 @@ export const CarouselItem: FC<{ product: IProduct }> = ({ product }) => {
 				}
 			</div>
 
-		</div>
+		</button>
 	);
 };
