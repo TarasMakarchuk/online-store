b72b756abc912865206d94cf1b42687cc7d965a5
Taras Makarchuk <2278452@gmail.com>
2023-03-23 21:20:33 +0200

    :zap: add product information

--- client/app/components/screens/product-details/product-card/ProductInformation.tsx
+++ client/app/components/screens/product-details/product-card/ProductInformation.tsx
@@ -1,9 +1,21 @@
 import { FC } from 'react';
+import Image from 'next/image';
+import { IProductDetails } from '@/types/product.interface';
+import styles from './ProductCard.module.scss';
 
-const ProductInformation: FC = () => {
+const ProductInformation: FC<IProductDetails> = ({ product }) => {
 	return (
-		<div>
-			<h1></h1> ProductInformation
+		<div className={styles.description}>
+			<h1>{ product.name }</h1>
+			<div>
+				<p>
+					{ product.description }
+				</p>
+			</div>
+			{ product.images.map(image => <button key={image}>
+					<Image src={image} alt='' width={100} height={100} />
+				</button>
+			) }
 		</div>
 	);
 };
