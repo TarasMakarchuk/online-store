13275bb5024bcda286a3571772fc59af36a6509c
Taras Makarchuk <2278452@gmail.com>
2023-03-28 19:19:41 +0300

    :construction: add  product module, add nestjs config and .env file

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const PORT = process.env.API_PORT || 8080;
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  await app.listen(5000);
+  await app.listen(PORT, () => {
+    Logger.log(`http://localhost:${PORT}`, `Server start on host`);
+  });
 }
+
 bootstrap();
